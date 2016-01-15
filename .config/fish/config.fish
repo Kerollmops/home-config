1ddd130d3af6f7cb9a2cf2b396f0987d8443dda7
Clement RENAULT <crenault@e3r1p6.42.fr>
2016-01-15 19:11:09 +0100

    ok handler handled

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,3 +12,19 @@ abbr --add ll="ls -la"
 
 # sublime text
 abbr --add st="sublime_text"
+
+function __fish_default_command_not_found_handler
+	echo "fish: Hoooo, nope! '$argv' doesn't exist !" >&2
+end
+
+# function __fish_default_command_not_found_handler
+#     echo "fish: Unknown command '$argv'" >&2
+# end
+
+#
+# Hook up the default as the principal command_not_found handler
+# in case we are not interactive
+#
+status -i; or function __fish_command_not_found_handler --on-event fish_command_not_found
+    __fish_default_command_not_found_handler $argv
+end
--- .config/fish/functions/__fish_command_not_found_handler.fish
+++
@@ -1,3 +0,0 @@
-function __fish_command_not_found_handler --on-event fish_command_not_found
-	echo "fish: Hoooo, nope! '$argv' doesn't exist !" >&2
-end
